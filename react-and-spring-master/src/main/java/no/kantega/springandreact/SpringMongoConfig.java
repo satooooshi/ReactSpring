61fedde22fa263fac5b5f34b8d0b47cb33121ed3
satoshi aikawa <satoshiaikawa@satoshi-no-MacBook-Pro.local>
2019-01-04 23:11:54 +0800

    create template

---
+++ react-and-spring-master/src/main/java/no/kantega/springandreact/SpringMongoConfig.java
@@ -0,0 +1,49 @@
+package no.kantega.springandreact;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
+
+import com.mongodb.reactivestreams.client.MongoClient;
+import com.mongodb.reactivestreams.client.MongoClients;
+
+@Configuration
+public class SpringMongoConfig extends AbstractReactiveMongoConfiguration {
+    private static final Logger log = LoggerFactory.getLogger(SpringMongoConfig.class);
+
+
+
+    @Value("${spring.data.mongodb.host}")
+    private String mongoHost;
+
+    @Value("${spring.data.mongodb.port}")
+    private String mongoPort;
+
+    @Value("${spring.data.mongodb.database}")
+    private String mongoDB;
+
+    @Override
+    protected String getDatabaseName() {
+        return mongoDB;
+    }
+
+    @Override
+    public MongoClient reactiveMongoClient() {
+        String myHost = System.getenv("MONGODB_HOST");
+        log.info("MONGODB_HOST="+myHost);
+        /*
+        log.setLevel(Level.TRACE); // ★デフォルトだと trace レベルは出力されないので、出力のレベルを TRACE にしている
+        log.trace("trace message");
+        log.debug("debug message");
+        log.info("info message");
+        log.warn("warn message");
+        log.error("error message");
+        */
+        return MongoClients.create("mongodb://"+(myHost==null ? mongoHost : myHost)+":"+mongoPort);
+    }
+
+}
